780d747325d662ff5436295714eb554990dfbc0c
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-05-17 05:44:55 +0900

    Update pow.h

--- src/pow.h
+++ src/pow.h
@@ -1,1 +1,15 @@
-int mypow(int,int,int);
+#include <stdio.h>
+
+int mypow(int x, int y,int d) {
+    if(y==0){
+        return 1;
+    }else{
+        int z = mypow(x,(y/2),d);
+        if(y % 2 == 1){
+            z = x*z*z;
+        }else{
+            z = z*z;
+        }
+        return z % d;
+    }
+}
