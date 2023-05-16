0584b067f1d05b7dcd7f9e85c95dcd05bf05a49c
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-05-17 05:17:38 +0900

    Create pow.c

---
+++ src/pow.c
@@ -0,0 +1,16 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+int mypow(int x, int y,int d) {
+    if(y==0){
+        return 1;
+    }else{
+        int z = mypow(x,(y/2),d);
+        if(y % 2 == 1){
+            z = x*z*z;
+        }else{
+            z = z*z;
+        }
+        return z % d;
+    }
+}
