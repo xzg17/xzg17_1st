f7b0b99916fcd4e7c724f0560fe75dac2fff3c0d
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-05-17 06:51:32 +0900

    Create func.h

---
+++ src/func.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int mypow(int x, int y,int d) {
+    if(y==0){
+        return 1;
+    }else{
+        int z = mypow(x,(y/2),d);
+        if(y % 2 == 1){
+            z = x*z*z;
+        }else{
+            z = z*z;
+        }
+        return z % d;
+    }
+}
